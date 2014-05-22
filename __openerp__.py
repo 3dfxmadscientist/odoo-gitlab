7b4130a6245e25479fe700f6385aac49f939c70d
Claudio <c.mignanti@gmail.com>
2014-05-22 16:03:48 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'GitLab Integration',
+    'version': '0.1',
+    'category': 'Project Management',
+    'sequence': 10,
+    'summary': 'test gitlab-integration',
+    'description': """
+test of test in test of the test
+    """,
+    'author': 'Claudio Mignanti',
+    'website': '',
+    'depends': [
+        'project',
+    ],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ project_gitlab.py
@@ -0,0 +1,12 @@
+ #-*- coding: utf-8 -*-
+
+from openerp.osv import fields, osv
+
+
+class project(osv.Model):
+    _inherit = "project.project"
+    _columns = {
+        'url_base': fields.text('gitlab_url_base'),
+        'prj_id': fields.integer('gitlab_prj_id'),
+        'private_token': fields.char('gitlab_private_token', size=21)
+    }

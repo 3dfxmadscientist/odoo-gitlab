de915ab47e219f87c2de95100d8090a499b1fcaa
Claudio <c.mignanti@gmail.com>
2014-05-22 18:02:44 +0200

    use same name mbho...

--- project_gitlab.py
+++ project_gitlab.py
@@ -8,9 +8,9 @@ class project_gitlab(osv.osv):
     _inherit = "project.project"
     
     _columns = {
-        'url_base': fields.text('gitlab_url_base'),
-        'prj_id': fields.integer('gitlab_prj_id'),
-        'private_token': fields.char('gitlab_private_token', size=21)
+        'url_base': fields.text('url_base'),
+        'prj_id': fields.integer('prj_id'),
+        'private_token': fields.char('private_token', size=21)
     }
 
 project_gitlab()
